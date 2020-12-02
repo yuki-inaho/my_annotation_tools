1bf60da115421ff67c633d16fa3a48a0ab60a9b6
yuki-inaho <yoshikawa@inaho.co>
2020-12-02 14:08:05 +0900

    Add a mkdir line

--- png2jpg.py
+++ png2jpg.py
@@ -13,6 +13,9 @@ SCRIPT_DIR = str(Path(__file__).parent)
 @click.option("--input-image-dir", "-i", default=f"{SCRIPT_DIR}/data/Image_raw")
 @click.option("--output-image-dir", "-o", default=f"{SCRIPT_DIR}/data/Image")
 def main(input_image_dir, output_image_dir):
+    output_image_dir_path = Path(output_image_dir)
+    if not output_image_dir_path.exists():
+        output_image_dir_path.mkdir()
     extf = [".jpg", ".png"]
     image_pathes = [path for path in Path(input_image_dir).glob("*") if path.suffix in extf]
     image_path_list = [str(image_path) for image_path in image_pathes]
