8fcd9008c5e8661232b2acfb6ee75127f9ff0caa
yuki-inaho <yoshikawa@inaho.co>
2021-02-26 13:22:41 +0900

    WIP

--- desktop_sa_to_coco_detection.py
+++ desktop_sa_to_coco_detection.py
@@ -4,6 +4,8 @@ import shutil
 import json
 from pathlib import Path
 
+
+import numpy as np
 from typing import List
 from tqdm import tqdm
 from enum import Enum
@@ -13,6 +15,7 @@ import pdb
 
 
 HOME_PATH = os.environ["HOME"]
+
 
 class Label(Enum):
     Asparagus = 1
@@ -26,14 +29,14 @@ class COCOInstanceAnnotation:
         image_name: str,
         image_size: ImageSize,
         annotation_id_list: List[int],
-        bb_obj_list: List[BoundingBox],
+        bb_object_list: List[BoundingBox],
         licence_id: int = 1,
     ):
         self._image_id = image_id
         self._image_name = image_name
         self._image_size = image_size
         self._annotation_id_list = annotation_id_list
-        self._bb_obj_list = bb_obj_list
+        self._bb_object_list = bb_object_list
         self._licence_id = licence_id
 
     @property
@@ -55,15 +58,29 @@ class COCOInstanceAnnotation:
                 {
                     "id": self._annotation_id_list[i],
                     "image_id": self._image_id,
-                    "segmentation": [self._bb_obj_list[i].bounding_box_polypoints],
+                    "segmentation": [self._bb_object_list[i].bounding_box_polypoints],
                     "iscrowd": 0,
-                    "bbox": [self._bb_obj_list[i].bounding_box_coco],
-                    "area": self._bb_obj_list[i].area,
-                    "category_id": self._bb_obj_list[i].category_id,
+                    "bbox": [self._bb_object_list[i].bounding_box_coco],
+                    "area": self._bb_object_list[i].area,
+                    "category_id": self._bb_object_list[i].category_id,
                 }
             )
         return dict_instances
 
+
+def extract_and_parse_sa_annotated_instance(annotation_sa_data, image_size: ImageSize) -> List[BoundingBox]:
+    instance_sa_list = annotation_sa_data["instances"]
+    bb_list = []
+    for instance_sa in instance_sa_list:
+        bb_obj = BoundingBox(image_size)
+        x_min = instance_sa["points"]["x1"]
+        y_min = instance_sa["points"]["y1"]
+        x_max = instance_sa["points"]["x2"]
+        y_max = instance_sa["points"]["y2"]
+        bb_obj.set_bounding_box_xyxy(x_min, y_min, x_max, y_max)
+        bb_obj.set_category_id(instance_sa["classId"])
+        bb_list.append(bb_obj)
+    return bb_list
 
 
 @click.command()
@@ -79,9 +96,10 @@ def main(input_dir_path, output_dir_path, image_width, image_height):
         shutil.rmtree(output_dir_path)
     output_dir_pathlib.mkdir()
 
-    input_image_size = ImageSize(image_width, image_height) # assume all images have the same image size
+    input_image_size = ImageSize(image_width, image_height)  # assume all images have the same image size
     input_image_path_list = get_image_pathes(input_dir_pathlib)
-    for input_image_path in tqdm(input_image_path_list):
+    current_annotation_data_count = 0
+    for image_index, input_image_path in enumerate(tqdm(input_image_path_list)):
         # Get image & annotation data path
         image_name = Path(input_image_path).name
         annotation_json_name = image_name + ".json"
@@ -89,22 +107,15 @@ def main(input_dir_path, output_dir_path, image_width, image_height):
 
         # Load annotation json
         annotation_sa_data = load_json(annotation_json_path)
+        bb_object_list = extract_and_parse_sa_annotated_instance(annotation_sa_data, input_image_size)
+        n_bounding_box = len(bb_object_list)
 
-        def extract_and_parse_sa_annotated_instance(annotation_sa_data, image_size: ImageSize) -> List[BoundingBox]:
-            instance_sa_list = annotation_sa_data["instances"]
-            bb_list = []
-            for instance_sa in instance_sa_list:
-                bb_obj = BoundingBox(image_size)
-                x_min = instance_sa["points"]["x1"]
-                y_min = instance_sa["points"]["y1"]
-                x_max = instance_sa["points"]["x2"]
-                y_max = instance_sa["points"]["y2"]
-                bb_obj.set_bounding_box_xyxy(x_min, y_min, x_max, y_max)
-                bb_obj.set_category_id(instance_sa["classId"])                
-                bb_list.append(bb_obj)
-            return bb_list
+        annotation_id_list = np.arange(n_bounding_box) + current_annotation_data_count
+        coco_instances_info = COCOInstanceAnnotation(image_index, image_name, input_image_size, annotation_id_list, bb_object_list)
+        current_annotation_data_count += n_bounding_box
 
-        sa = extract_and_parse_sa_annotated_instance(annotation_sa_data, input_image_size)
+        # @
+
         output_image_path = str(Path(output_dir_path, image_name))
         shutil.copy(input_image_path, output_image_path)
 
