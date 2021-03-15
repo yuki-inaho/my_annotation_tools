811f4c6e0d053099695f3e7a6ed87a9b05710f38
yuki-inaho <yoshikawa@inaho.co>
2021-03-15 11:46:46 +0900

    Add a option

--- duplicate_image_removal_with_ssim.py
+++ duplicate_image_removal_with_ssim.py
@@ -13,14 +13,15 @@ import matplotlib.pyplot as plt
 def draw_ssim_hist(ssim_list):
     n, bins = np.histogram(ssim_list, bins=100, range=(0, 1.0), weights=None, normed=False)
     fig = plt.figure()
-    plt.step(bins[:-1], n, where='post')
+    plt.step(bins[:-1], n, where="post")
     fig.savefig("ssim.png")
 
 
 @click.command()
 @click.option("--input-image-dir", "-i", required=True)
 @click.option("--output-image-dir", "-o", required=True)
-def main(input_image_dir, output_image_dir):
+@click.option("--ssim-threshold", "-s", default=0.28)
+def main(input_image_dir, output_image_dir, ssim_threshold):
     input_image_dir_pathlib = Path(input_image_dir)
     output_image_dir_pathlib = Path(output_image_dir)
     mkdir_from_path(output_image_dir)
@@ -32,12 +33,12 @@ def main(input_image_dir, output_image_dir):
         prev_image = cv2.imread(input_image_pathes[i - 1])
         curr_image = cv2.imread(input_image_pathes[i])
         ssim_value = SSIM(prev_image, curr_image, max_value=None, win_size=7)
-        if ssim_value < 0.28:
+        if ssim_value < ssim_threshold:
             input_image_pathlib = Path(input_image_pathes[i])
             input_image_name = input_image_pathlib.name
             output_image_pathlib = output_image_dir_pathlib.joinpath(input_image_name)
             shutil.copy(str(input_image_pathlib), str(output_image_pathlib))
-        #ssim_list.append(ssim_value)
+        # ssim_list.append(ssim_value)
 
 
 if __name__ == "__main__":
