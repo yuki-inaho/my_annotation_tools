34b0121598b1ef05ec7eaaa3142b06163074caf0
yuki-inaho <yoshikawa@inaho.co>
2021-03-30 17:09:24 +0900

    Update

--- add_class_names_to_desktop_sa_json.py
+++ add_class_names_to_desktop_sa_json.py
@@ -2,8 +2,9 @@ import os
 import click
 import shutil
 from pathlib import Path
+
 from tqdm import tqdm
-from scripts.utils import load_json, dump_json, get_image_pathes, mkdir_from_path
+from scripts.utils import read_json, write_json, get_image_pathes, mkdir_from_path
 from scripts.category import AnnotationClassManager
 
 
@@ -11,14 +12,16 @@ HOME_PATH = os.environ["HOME"]
 
 
 @click.command()
-@click.option("--input-dir-path", "-i", default=f"{HOME_PATH}/superannotate_preupload/aspara_root_scene")
-@click.option("--output-dir-path", "-o", default=f"{HOME_PATH}/superannotate_preupload/converted")
-@click.option("--classes-json", "-c", default=f"json/classes.json")
-def main(input_dir_path, output_dir_path, classes_json):
+@click.option("--input-dir-path", "-i", default=f"{HOME_PATH}/superannotate_preupload/sa_desktop")
+@click.option("--output-dir-path", "-o", default=f"{HOME_PATH}/superannotate_preupload/sa_desktop_modified")
+@click.option("--classes-json", "-c", default=f"cfg/classes.json")
+@click.option("--flag-mkdir", "-m", is_flag=True)
+def main(input_dir_path, output_dir_path, classes_json, flag_mkdir):
     # Set input
     input_dir_pathlib = Path(input_dir_path)
-    mkdir_from_path(output_dir_path)
     annotation_class_manager = AnnotationClassManager(classes_json)
+    if flag_mkdir:
+        mkdir_from_path(output_dir_path)
 
     # Get Image pathes from input image directory
     input_image_path_list = get_image_pathes(input_dir_pathlib)
@@ -27,23 +30,22 @@ def main(input_dir_path, output_dir_path, classes_json):
         image_name = Path(input_image_path).name
         annotation_input_json_name = image_name + ".json"
         annotation_input_json_path = str(Path(input_dir_path, annotation_input_json_name))
-        annotation_json = load_json(annotation_input_json_path)
+        annotation_json = read_json(annotation_input_json_path)
         n_instances = len(annotation_json["instances"])
-
-        real_instance_count = 0
         for i in range(n_instances):
             instance_info_ith = annotation_json["instances"][i]
-            if instance_info_ith["classId"] == -1:
-                continue
             instance_info_ith["className"] = annotation_class_manager.id2name(instance_info_ith["classId"])
-            annotation_json["instances"][real_instance_count] = instance_info_ith
-            real_instance_count += 1
+            annotation_json["instances"][i] = instance_info_ith
 
         # Dump image and annotation data
         output_image_path = str(Path(output_dir_path, image_name))
         output_annotation_path = str(Path(output_dir_path, annotation_input_json_name))
-        shutil.copy(input_image_path, output_image_path)
-        dump_json(output_annotation_path, annotation_json)
+
+        if flag_mkdir:
+            shutil.copy(input_image_path, output_image_path)
+            write_json(output_annotation_path, annotation_json)
+        else:
+            write_json(annotation_input_json_path, annotation_json)
 
 
 if __name__ == "__main__":
--- rename_annotation_files_desktop_sa.py
+++ rename_annotation_files_desktop_sa.py
@@ -1,47 +1,41 @@
-import cv2
 import os
 import click
 import shutil
 from pathlib import Path
 from tqdm import tqdm
-from scripts.utils import get_image_pathes, load_json, dump_json
+from scripts.utils import get_image_pathes, mkdir_from_path
 
 
 HOME_PATH = os.environ["HOME"]
 
 
 @click.command()
-@click.option("--input-dir-path", "-i", default=f"{HOME_PATH}/superannotate_preupload/converted")
-@click.option("--output-dir-path", "-o", default=f"{HOME_PATH}/superannotate_preupload/vector")
+@click.option("--input-dir-path", "-i", default=f"{HOME_PATH}/superannotate_preupload/sa_desktop_modified")
+@click.option("--output-dir-path", "-o", default=f"")
 def main(input_dir_path, output_dir_path):
-    # Set input 
+    # Set input
     input_dir_pathlib = Path(input_dir_path)
-    output_dir_pathlib = Path(output_dir_path)
-    if output_dir_pathlib.exists():
-        shutil.rmtree(output_dir_path)
-    output_dir_pathlib.mkdir()
+    is_output_same_dir = output_dir_path == ""
+    if not is_output_same_dir:
+        mkdir_from_path(output_dir_path)
 
     # Get Image pathes from input image directory
     input_image_path_list = get_image_pathes(input_dir_pathlib)
     for input_image_path in tqdm(input_image_path_list):
-        image = cv2.imread(input_image_path)
-        image_height, image_width, _ = image.shape
-
         # Get image & annotation path info
         image_name = Path(input_image_path).name
-        image_ext = Path(input_image_path).suffix
         annotation_input_json_name = image_name + ".json"
         annotation_input_json_path = str(Path(input_dir_path, annotation_input_json_name))
-        annotation_data = load_json(annotation_input_json_path)
-        annotation_data["metadata"]["width"] = image_width
-        annotation_data["metadata"]["height"] = image_height
 
         # Dump image and annotation data
-        output_image_path = str(Path(output_dir_path, image_name))
-        output_annotation_path = str(Path(output_dir_path, image_name + "___objects.json"))
-
-        shutil.copy(input_image_path, output_image_path)
-        dump_json(output_annotation_path, annotation_data)
+        if not is_output_same_dir:
+            output_image_path = str(Path(output_dir_path, image_name))
+            output_annotation_path = str(Path(output_dir_path, image_name + "___objects.json"))
+            shutil.copy(input_image_path, output_image_path)
+            shutil.copy(annotation_input_json_path, output_annotation_path)
+        else:
+            output_annotation_path = str(Path(input_dir_path, image_name + "___objects.json"))
+            shutil.move(annotation_input_json_path, output_annotation_path)
 
 
 if __name__ == "__main__":
